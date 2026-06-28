f51bcd4353b6f1cfec66a02aaa54a3e0fb86781d
new178901-ui <new178901@gmail.com>
2026-06-28 15:54:24 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-        "https://pages.razorpay.com/avadale",
+        "https://pages.razorpay.com/Gift2DS",
     }
     urlIndex   uint64
     proxyIndex uint64
