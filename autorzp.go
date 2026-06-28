c2a3ed3d7dbda203fbe8a8fe310633615b40d740
new178901-ui <new178901@gmail.com>
2026-06-28 15:56:19 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-        "https://pages.razorpay.com/Gift2DS",
+        "https://pages.razorpay.com/pl_C0rYjVEXmkqYVC/view",
     }
     urlIndex   uint64
     proxyIndex uint64
