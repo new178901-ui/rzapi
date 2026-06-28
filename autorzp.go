ee8204048657fbd6ebce938fe3857d531c811550
new178901-ui <new178901@gmail.com>
2026-06-28 17:21:02 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-        "https://pages.razorpay.com/pl_C0rYjVEXmkqYVC/view",
+        "https://pages.razorpay.com/paytomitzvahLtd",
     }
     urlIndex   uint64
     proxyIndex uint64
