aa9c22a372b33e44bb6b07611d937205de4da0b8
new178901-ui <new178901@gmail.com>
2026-07-10 11:38:57 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -40,7 +40,7 @@ const (
 var (
     razorpayURLs = []string{
         
-        "https://pages.razorpay.com/phpaynow",
+        "https://pages.razorpay.com/pl_JGM8Zagxs1rbT8/view",
         
         
         
