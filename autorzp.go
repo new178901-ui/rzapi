21a9aa64bb02f79b28dd30faf3a7360253b71c58
new178901-ui <new178901@gmail.com>
2026-07-10 11:10:54 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -101,7 +101,7 @@ func loadProxies(filepath string) []string {
 }
 
 func getNextProxy(proxyList []string) string {
-    return "GLLwbIADbhlmzQp:G9TqW041TQraT2o@thehub.proxy-cheap.com:8080"
+    return "http://GLLwbIADbhlmzQp:G9TqW041TQraT2o@thehub.proxy-cheap.com:8080"
     
     if len(proxyList) == 0 {
         return ""
