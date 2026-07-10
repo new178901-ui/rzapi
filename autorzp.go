7e732fe2f8c8c617dcf5b7e60c75c7c602774182
new178901-ui <new178901@gmail.com>
2026-07-10 11:36:48 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -40,7 +40,7 @@ const (
 var (
     razorpayURLs = []string{
         
-        "https://pages.razorpay.com/indiumlaw",
+        "https://pages.razorpay.com/phpaynow",
         
         
         
