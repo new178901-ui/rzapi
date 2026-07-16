dfd787bd23d119d26e522985e2034c8c327ea4a7
new178901-ui <new178901@gmail.com>
2026-07-16 10:54:16 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,11 +39,8 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/indiumlaw",
-        "https://pages.razorpay.com/WesternRef",
-        "https://pages.razorpay.com/psnapayment",
-        "https://pages.razorpay.com/devotionalpointpayment",
-        "https://pages.razorpay.com/psnapayment",
+           "https://pages.razorpay.com/AHC-Kolkata",
+        
         
            
         
