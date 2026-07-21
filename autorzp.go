1f1efa3ab94e4cf4ac685a0978fda6d0c518db71
new178901-ui <new178901@gmail.com>
2026-07-21 23:59:22 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-        "https://pages.razorpay.com/pl_NfrG1qOGaFYl5V/view",
+        "https://pages.razorpay.com/theothersongPaymentINR",
         
           
         
