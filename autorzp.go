3a24f0fd2b30779278f790c29f507a57f303021e
new178901-ui <new178901@gmail.com>
2026-07-28 11:03:20 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/aafmcoursefee",
+           "https://pages.razorpay.com/indiatourplanner",
 
         
         
