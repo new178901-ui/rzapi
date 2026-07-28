d4b5bf3f9749b3a912a1a485c982c588a470fd20
new178901-ui <new178901@gmail.com>
2026-07-28 11:24:54 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -34,7 +34,7 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/XergY-full-payment",
+           "https://pages.razorpay.com/wabisabiquickpay",
 
         
         
