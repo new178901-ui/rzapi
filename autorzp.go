d3c9a207a00095918d224915c0f6d52e597060cc
new178901-ui <new178901@gmail.com>
2026-07-28 10:26:10 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/pl_QV7V9tQXvWF9hW/view",
+           "https://pages.razorpay.com/techdost",
 
         
         
