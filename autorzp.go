2463ba9b6dc4aa2fdeadae949eb8ff29cf20876d
new178901-ui <new178901@gmail.com>
2026-07-28 11:06:58 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -39,7 +39,7 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/FACTSTRANSCRIPTS",
+           "https://pages.razorpay.com/theparkholidays",
 
         
         
