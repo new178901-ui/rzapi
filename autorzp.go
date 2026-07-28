234312628c52b25defd3c8732d67edfc05f12562
new178901-ui <new178901@gmail.com>
2026-07-28 11:14:35 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -34,7 +34,7 @@ const (
 
 var (
     razorpayURLs = []string{
-           "https://pages.razorpay.com/toppersnotespayment",
+           "https://pages.razorpay.com/nirmitifoundation",
 
         
         
