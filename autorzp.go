675460083197a479c6c23be3d56f64aad1cb9c9f
new178901-ui <new178901@gmail.com>
2026-07-29 20:23:17 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -99,7 +99,7 @@ func loadProxies(filepath string) []string {
 }
 
 func getNextProxy(proxyList []string) string {
-    return "http://purevpn0s551451:9dpdlc2nfxgj@px022507.pointtoserver.com:10780"
+    return ""
 
     
     if len(proxyList) == 0 {
