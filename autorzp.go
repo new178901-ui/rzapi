cbfe0f98d18f7c601aa9ba0b1ee87166bc120467
new178901-ui <new178901@gmail.com>
2026-08-01 17:41:46 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -34,7 +34,10 @@ const (
 
 var (
     razorpayURLs = []string{
-         "https://pages.razorpay.com/instantpayment",
+         "https://pages.razorpay.com/quickpayment",
+        "https://pages.razorpay.com/paymentorder",
+        
+        
         
            
         
