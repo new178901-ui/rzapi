5a6acde88fcae3bcd323af6ab389e7b6068126d3
new178901-ui <new178901@gmail.com>
2026-08-02 09:48:55 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -99,11 +99,11 @@ func loadProxies(filepath string) []string {
 }
 
 func getNextProxy(proxyList []string) string {
-    return "http://brd.superproxy.io:44445:brd-customer-hl_811cf298-zone-datacenter_proxy1:i8xrcc36ld0i"
+    return "http://brd-customer-hl_811cf298-zone-datacenter_proxy1:i8xrcc36ld0i@brd.superproxy.io:44445"
 
     
     if len(proxyList) == 0 {
-        return "http://brd.superproxy.io:44445:brd-customer-hl_811cf298-zone-datacenter_proxy1:i8xrcc36ld0i"
+        return "http://brd-customer-hl_811cf298-zone-datacenter_proxy1:i8xrcc36ld0i@brd.superproxy.io:44445"
     }
     idx := atomic.AddUint64(&proxyIndex, 1) - 1
     return proxyList[idx%uint64(len(proxyList))]
