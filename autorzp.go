615bc3be980255261c24b6e5f1bd88a4b71d3c1e
new178901-ui <new178901@gmail.com>
2026-08-02 09:37:37 +0530

    Update autorzp.go

--- autorzp.go
+++ autorzp.go
@@ -99,11 +99,11 @@ func loadProxies(filepath string) []string {
 }
 
 func getNextProxy(proxyList []string) string {
-    return ""
+    return "http://4933:ei95h8eMH8QY@p104.instantproxies.com:9046"
 
     
     if len(proxyList) == 0 {
-        return ""
+        return "http://4933:ei95h8eMH8QY@p104.instantproxies.com:9046"
     }
     idx := atomic.AddUint64(&proxyIndex, 1) - 1
     return proxyList[idx%uint64(len(proxyList))]
